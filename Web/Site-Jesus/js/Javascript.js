b53fd427200e03dc03b219f2a9e937f06ac362b0
Jefferson <kio123457@gmail.com>
2021-06-18 20:07:26 -0300

    Projeto pessoal - The Timeline of their Fame de Jesus Cristo

---
+++ Web/Site-Jesus/js/Javascript.js
@@ -0,0 +1,32 @@
+/* 'use strict';
+
+let elLista = document.querySelectorAll(".ul-explanation li");
+let lista = document.querySelector(".ul-explanation");
+let track = document.querySelector(".contents-explanation");
+let glide = document.querySelector(".explanation");
+let arrow = document.querySelector(".glide__arrows");
+
+if(window.innerWidth <= 1150){
+    elLista[0].classList.add("glide__slide");
+    elLista[1].classList.add("glide__slide");
+
+    lista.classList.add("glide__slides");
+    track.classList.add("glide__track");
+    glide.classList.add("glide");
+
+    arrow.style.display = "block";
+}else{
+    elLista[0].classList.remove("glide__slide");
+    elLista[1].classList.remove("glide__slide");
+
+    lista.classList.remove("glide__slides");
+    track.classList.remove("glide__track");
+    glide.classList.remove("glide");
+
+}
+ */
+/* window.addEventListener('resize', (e) => {
+
+
+
+}) */
